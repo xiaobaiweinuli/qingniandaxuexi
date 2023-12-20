0a1f408e606f24ba6ac4587394613a015cfa2f05
xiaobaiweinuli <94781176+xiaobaiweinuli@users.noreply.github.com>
2023-12-20 16:27:52 +0800

    Update xizhi.sh

--- xizhi.sh
+++ xizhi.sh
@@ -2,7 +2,7 @@ title="浅蓝色"
 content="[大学习更新](http://hnqndaxuexi.dahejs.cn/study/studyList)"
 
 # 使用环境变量 xizhi_token
-xizhi_token="${{ secrets.XIZHI_TOKEN }}"
+xizhi_token="$XIZHI_TOKEN"
 
 # 使用 curl 发送 POST 请求
 push_response=$(curl -s -X POST "https://xizhi.qqoq.net/${xizhi_token}.channel?" -d "title=${title}&content=${content}")
